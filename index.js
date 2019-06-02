e0fa129dd06ae5ea8bae5b81567f32b04f1d76aa
Saurav <kuwarsaurav21@gmail.com>
2019-06-02 22:18:02 +0545

    working on file upload

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const Hapi = require('hapi');
+const Inert = require('inert');
+const fs = require('fs');
 
 // Create a server with a host and port
 const server = Hapi.server({
@@ -26,9 +28,54 @@ server.route([{
     }
 ]);
 
+const handleFileUpload = file => {
+    return new Promise((resolve, reject) => {
+        const filename = file.hapi.filename;
+        const data = file._data;
+        fs.writeFile('./upload/' + filename, data, err => {
+            if (err) {
+                reject(err);
+            }
+            resolve({
+                message: 'Upload successfully!'
+            });
+        });
+    });
+};
+
+server.route({
+    path: '/upload',
+    method: 'POST',
+    options: {
+        payload: {
+            output: 'stream',
+        }
+    },
+    handler: (req, h) => {
+        const {
+            payload
+        } = req;
+        const response = handleFileUpload(payload.file);
+        return response;
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/upload/{file*}',
+    handler: {
+        directory: {
+            path: 'upload'
+        }
+    }
+});
+
+
 // Start the server
 const start = async () => {
     try {
+        await server.register(Inert);
+
         await server.start();
     } catch (err) {
         console.log(err);
